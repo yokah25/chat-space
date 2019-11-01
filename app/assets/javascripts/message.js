349deba328ac10c008ba334b2df00c46a8b66dfb
yokah25 <yokah25iruka@gmail.com>
2019-11-01 23:01:47 +0900

    非同期実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,53 @@
+$(function(){
+  
+  function buildMessage(message){
+    var addImage = (message.image !== null) ? `<img class = "lower-message__image", src="${message.image}">` : ''
+
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                    ${ message.name }
+                    </div>
+                    <div class="upper-message__date">
+                    ${ message.date }
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                    ${ message.content }
+                    </p>
+                    ${ addImage }
+                  </div>
+                </div>`
+    return html
+  };
+
+  $(".new_message").on('submit', function(e){
+    e.preventDefault();
+    var url = $(this).attr('action');
+    var formData = new FormData(this);
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData, 
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(message){
+      var html = buildMessage(message);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
+
+    .always(() => {
+      $(".form__submit").removeAttr("disabled");
+    })
+  });
+});
