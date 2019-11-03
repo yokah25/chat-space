8fd4d58178f697cd1f831953fd4cc49353d5eadd
yokah25 <yokah25iruka@gmail.com>
2019-11-03 13:45:24 +0900

    インクリメンタルサーチ

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,15 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
-    
   end
   
   def new
     @group = Group.new
     @group.users << current_user
+  end
+
+  def edit
   end
 
   def create
